8330bb15629b88840ff72e10c6aa12ad5a8deba4
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-21 23:43:08 +0900

    Update slack_session.py

--- vmcjp/slack_session.py
+++ vmcjp/slack_session.py
@@ -12,6 +12,18 @@ logger.setLevel(logging.INFO)
 
 #EXPECTED_TOKEN = os.environ["token"]
 #BOT_OAUTH_TOKEN = os.environ["bot_token"]
+def read_db(event):
+    db = dbutils.DocmentDb(
+        constant.S3_CONFIG,
+        constant.USER_DB,
+        constant.USER_COLLECTION
+    )
+    
+    past = (
+        datetime.datetime.now() - datetime.timedelta(minutes=5)
+    ).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    
+    db.find({"start_time": {"$gt": past}})
 
 def write_db(event):
     db = dbutils.DocmentDb(
