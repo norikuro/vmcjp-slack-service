548ea96352fdf0bbf1c98305338d8ee79e12c897
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-21 18:27:24 +0900

    Update slack_session.py

--- vmcjp/slack_session.py
+++ vmcjp/slack_session.py
@@ -10,7 +10,7 @@ from vmcjp.utils import constant
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-EXPECTED_TOKEN = os.environ["token"]
+#EXPECTED_TOKEN = os.environ["token"]
 BOT_OAUTH_TOKEN = os.environ["bot_token"]
 
 def write_db(event):
