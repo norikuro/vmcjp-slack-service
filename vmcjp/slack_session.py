9822c44278e58416e794ddf3ac2d8492978fecca
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-21 17:35:09 +0900

    Update slack_session.py

--- vmcjp/slack_session.py
+++ vmcjp/slack_session.py
@@ -1,6 +1,7 @@
 import json
 import os
 import logging
+import datetime
 
 from vmcjp.utils.slack_post import post
 from vmcjp.utils import dbutils
@@ -11,3 +12,12 @@ logger.setLevel(logging.INFO)
 
 EXPECTED_TOKEN = os.environ["token"]
 BOT_OAUTH_TOKEN = os.environ["bot_token"]
+
+def write_db(user, data):
+    db = dbutils.DocmentDb(
+        constant.S3_CONFIG,
+        constant.USER_DB,
+        constant.USER_COLLECTION
+    )
+    data.update({"start_time": datetime.datetime.now()})
+    db.upsert({"_id": user}, {"$set": data})
