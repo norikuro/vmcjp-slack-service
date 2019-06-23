ce5e5c83134189a11cc10de61190da2185d3dfe7
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-23 16:28:56 +0900

    Update slack_session.py

--- vmcjp/slack_session.py
+++ vmcjp/slack_session.py
@@ -37,6 +37,34 @@ def write_db(event):
 #    logging.info(event)
     db.upsert({"_id": event.pop("user")}, {"$set": event})
     
+def event_handler(event):
+    if "create sddc" in text:
+        data["text"] = "OK, starting create sddc wizard."
+        response = post(url, data, BOT_OAUTH_TOKEN)
+        data["text"] = "This conversation will end with typing `cancel` or doing nothing within 5 minutes"
+        response = post(url, data, BOT_OAUTH_TOKEN)
+        data["text"] = "Please enter SDDC name"
+        response = post(url, data, BOT_OAUTH_TOKEN)
+        le.update({"event_type": "create_sddc"})
+        call_lambda("slack_session", le)
+    elif "cancel" in text:
+        data["text"] = "OK, create SDDC has cenceled."
+        response = post(url, data, BOT_OAUTH_TOKEN)
+        le.update({"event_type": "cancel"})
+        call_lambda("slack_session", le)
+    elif text.find(" ") != -1:
+        data["text"] = event
+        response = post(url, data, BOT_OAUTH_TOKEN)
+    elif is_valid_network(text):
+        data["text"] = text
+        response = post(url, data, BOT_OAUTH_TOKEN)
+    else:
+        data["text"] = "Single host or Multi host?"
+        response = post(url, data, BOT_OAUTH_TOKEN)
+        le.update({"event_type": "sddc_name"})
+        call_lambda("slack_session", le)
+#    logging.info(response.read())
+
 def lambda_handler(event, context):
 #    logging.info(event)
     
