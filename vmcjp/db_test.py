a97baacefd1feeae2cae9ffebf1898bead4e7c7a
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-23 20:17:54 +0900

    Update db_test.py

--- vmcjp/db_test.py
+++ vmcjp/db_test.py
@@ -6,16 +6,16 @@ class Test(object):
   def db(self):
     db = dbutils.DocmentDb("vmcjp/s3config.json", "user_db", "user_collection")
     collection = db.get_collection()
-#    collection.remove()
-    delta = datetime.datetime.now() - datetime.timedelta(minutes=5)
-    past = delta.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    collection.remove()
+#    delta = datetime.datetime.now() - datetime.timedelta(minutes=5)
+#    past = delta.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 #    now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-    print(past)
-    col = collection.find({"start_time": {"$gt": past}})
+#    print(past)
+#    col = collection.find({"start_time": {"$gt": past}})
 #    col = collection.find()
-    print(col.count())
-    for data in col:
-      print(data)
+#    print(col.count())
+#    for data in col:
+#      print(data)
   
 def main():
   test = Test()
