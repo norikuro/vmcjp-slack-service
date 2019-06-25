e0ca439b30f9a8dad18f255c6c659a84b5f6a2bc
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-25 23:57:31 +0900

    Update slack_interactive.py

--- vmcjp/slack_interactive.py
+++ vmcjp/slack_interactive.py
@@ -17,16 +17,11 @@ def interactive_handler(event):
     db = dbutils2.DocmentDb(event["db_url"], constant.USER)
     
     user_id = event["user_id"]
-#    text = event["text"]
     url = event["response_url"]
-    bot_token = event["bot_token"]
     token = event["token"]
     org_id = event["org_id"]
     callback_id = event["callback_id"]
     event_response = event["response"]
-    logging.info(callback_id)
-    logging.info(event_response)
-    logging.info(user_id)
     
     data = {
         "token": event["token"],
@@ -34,20 +29,16 @@ def interactive_handler(event):
     }
     
     result = db.read_event_db(event["user_id"])
-    logging.info(result)
     if result is None:
       if callback_id == "create_sddc":
         if event_response == "yes":
             data["text"] = "Please enter SDDC name"
-#            response = post(url, data, bot_token)
             response = post_to_response_url(url, data)
-            logging.info(response.read())
+            db.write_event_db(user_id, {"command": "create_sddc"})
             return
         else:
             data["text"] = "OK, create SDDC has cenceled."
-#            response = post(url, data, bot_token)
             response = post_to_response_url(url, data)
-            logging.info(response.read())
             return
     else:
         logging.info("something wrong!!!")
