dcdae04181031228992833cee4cbb5f8e0891a29
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-25 22:03:21 +0900

    Update slack_interactive.py

--- vmcjp/slack_interactive.py
+++ vmcjp/slack_interactive.py
@@ -1,7 +1,7 @@
 help_message = "May I help you? please type `help` command."
 
 TEST_ORG_ID = os.environ["test_org"] #for test
-BUTTON = "vmcjp/precheck_button.json"
+#BUTTON = "vmcjp/precheck_button.json"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
