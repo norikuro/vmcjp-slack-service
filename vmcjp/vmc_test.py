213b277496eb4c82b2e387b0ffe2bc4ed7687711
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-26 17:35:23 +0900

    Update vmc_test.py

--- vmcjp/vmc_test.py
+++ vmcjp/vmc_test.py
@@ -1,1 +1,27 @@
-aa
+import json
+import atexit
+import requests
+
+from vmware.vapi.vmc.client import create_vmc_client
+from vmcjp.utils.metadata import get_members
+
+TEST_ORG_ID = os.environ["test_org"] #for test
+
+class Test(object):
+  def get_vmc_client(token):
+    session = requests.Session()
+    vmc_client = create_vmc_client(token, session=session)
+    atexit.register(session.close)
+    return vmc_client
+
+def main():
+  f = json.load(open(constant.S3_CONFIG, 'r'))
+  j = read_json_from_s3(f["bucket"], f["config"])
+  
+  test = Test()
+  vmc_client = test.get_vmc_client(j["token"])
+  print(get_members(vmc_client.orgs.account_link))
+  print(vmc_client.orgs.account_link)
+
+if __name__ == '__main__':
+  main()
