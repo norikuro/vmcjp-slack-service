e566ab9394027aa765fecefbf108a371314c6bcf
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-26 21:10:46 +0900

    Update slack_interactive.py

--- vmcjp/slack_interactive.py
+++ vmcjp/slack_interactive.py
@@ -18,8 +18,6 @@ AWS_ID = os.environ["aws_id"] #for internal use
 ACCOUNT_BUTTON = constant.BUTTON_DIR + "account_button.json"
 REGION_BUTTON = constant.BUTTON_DIR + "region_button.json"
 
-post_url = "https://slack.com/api/chat.postMessage"
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -105,15 +103,15 @@ def interactive_handler(event):
             response = post_to_response_url(event["response_url"], data)
         else:
             data["text"] = "Please enter CIDR block for management subnet."
-            response = post(post_url, data, data["bot_token"])
+            response = post(data["post_url"], data, data["bot_token"])
             data["text"] = "/23 is max 27 hosts, /20 is max 251, /16 is 4091."
-            response = post(post_url, data, data["bot_token"])
+            response = post(data["post_url"], data, data["bot_token"])
             data["text"] = "You can not use 10.0.0.0/15 and 172.31.0.0/16 which are reserved."
-            response = post(post_url, data, data["bot_token"])
+            response = post(data["post_url"], data, data["bot_token"])
         db.write_event_db(user_id, {"command": "link_aws", "num_hosts": 1})
     elif event["callback_id"] == "region":
         data["text"] = "Please enter SDDC name"
-        response = post(post_url, data, data["bot_token"])
+        response = post(data["post_url"], data, data["bot_token"])
         db.write_event_db(user_id, {"command": "region", "region": event["response"]})
     elif event["callback_id"] == "aws_account":
         data["text"] = "Please select VPC."
