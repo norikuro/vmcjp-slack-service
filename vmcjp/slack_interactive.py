8eb72747adaebd2e1734efd2663cffae22fdb075
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-27 23:45:43 +0900

    Update slack_interactive.py

--- vmcjp/slack_interactive.py
+++ vmcjp/slack_interactive.py
@@ -196,7 +196,6 @@ def interactive_handler(event):
         return
     elif event["callback_id"] == "single_multi":
         if "single" in event["response"]:
-            logging.info("!!!singleeeeeeee!!!")
             post_button(event, LINK_AWS_BUTTON)
         else:
             post_option(
