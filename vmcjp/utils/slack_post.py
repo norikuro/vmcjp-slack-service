7de83d2ed379873a7343eeeafccdb957eff92c8d
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-27 12:13:50 +0900

    Update slack_post.py

--- vmcjp/utils/slack_post.py
+++ vmcjp/utils/slack_post.py
@@ -41,3 +41,16 @@ def post_to_webhook(url, data):
     )
     
     return urllib2.urlopen(request)
+
+def post_text(event, text, reply=True):
+    data = {
+        "token": event["token"],
+        "channel": event["channel"],
+        "text": text
+    }
+    
+    if reply:
+        response = post_to_response_url(event["response_url"], data)
+    else:
+        response = post(event["post_url"], data, event["bot_token"])
+    return response
