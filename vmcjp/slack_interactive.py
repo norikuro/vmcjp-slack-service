4877e4a10b2160db906584b6de9ff847ee4d91fe
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-27 22:36:19 +0900

    Update slack_interactive.py

--- vmcjp/slack_interactive.py
+++ vmcjp/slack_interactive.py
@@ -8,7 +8,7 @@ from vmware.vapi.vmc.client import create_vmc_client
 
 from vmcjp.utils import dbutils2
 from vmcjp.utils import constant
-from vmcjp.utils.slack_post import post_text, post_option
+from vmcjp.utils.slack_post import post_text, post_option, post_button
 
 help_message = "May I help you? please type `help`."
 
