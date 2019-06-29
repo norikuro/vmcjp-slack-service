5569e69008acdd07e403decb5e94f9639795a789
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-29 10:59:01 +0900

    Update slack_event.py

--- vmcjp/slack_event.py
+++ vmcjp/slack_event.py
@@ -150,7 +150,7 @@ def event_handler(event):
             if result["command"] == "link_aws" or "subnet":
                 logging.info("pre event!!! " + json.dumps(event))
                 logging.info("result!!! " + json.dumps(result))
-                logging.info("!!field button, create, bot")
+                logging.info("result!!! type" + type(result))
                 post_field_button(
                     event.update(result), 
                     CREATE_BUTTON, 
