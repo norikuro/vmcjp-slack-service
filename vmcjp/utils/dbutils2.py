af615f55ff66b48a4454e369c73d1ac0110556a2
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-02 11:30:01 +0900

    Update dbutils2.py

--- vmcjp/utils/dbutils2.py
+++ vmcjp/utils/dbutils2.py
@@ -2,33 +2,35 @@ import json
 import pymongo
 import datetime
 
+from vmcjp.utils import constant
+
 class DocmentDb(object):
-    def __init__(self, url, db_name): 
+    def __init__(self, url): 
         self.client = pymongo.MongoClient(url)
-        self.db = self.client["{}_db".format(db_name)]
-        self.collection = self.db["{}_collection".format(db_name)]
+        self.event_db = self.client[constant.USER_DB]
+        self.event_col = self.db[constant.USER_COLLECTION]
     
     def get_client(self):
       return self.client
 
-    def get_db(self):
-        return self.db
+#    def get_db(self):
+#        return self.db
 
-    def get_collection(self):
-        return self.collection
+#    def get_collection(self):
+#        return self.collection
   
-    def upsert(self, query, update_data):
-        self.collection.update(query, update_data, upsert=True)
+#    def upsert(self, query, update_data):
+#        self.collection.update(query, update_data, upsert=True)
 
-    def find_with_fields(self, query, fields):
-        return self.collection.find(query, fields)[0]
+#    def find_with_fields(self, query, fields):
+#        return self.collection.find(query, fields)[0]
   
-    def find(self, query):
-        cur = self.collection.find(query)
-        if cur.count() != 0:
-            return cur[0]
-        else:
-            return
+#    def find(self, query):
+#        cur = self.collection.find(query)
+#        if cur.count() != 0:
+#            return cur[0]
+#        else:
+#            return
 
     def remove(self, data_to_remove):
         self.collection.remove(data_to_remove)
@@ -37,15 +39,18 @@ class DocmentDb(object):
         past = (
           datetime.datetime.now() - datetime.timedelta(minutes=minutes)
         ).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-
-        result = self.find({"start_time": {"$gt": past}, "_id": user_id})
-        return result
+        
+        cur = self.event_db.find({"start_time": {"$gt": past}, "_id": user_id})
+        if cur.count() != 0:
+            return cur[0]
+        else:
+            return
 
     def write_event_db(self, user_id, data):
         now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         data.update({"start_time": now})
         
-        self.upsert({"_id": user_id}, {"$set": data})
+        self.event_db.update({"_id": user_id}, {"$set": data}, upsert=True)
 
     def delete_event_db(self, user_id):
-        self.remove({"_id": user_id})
+        self.event_db.remove({"_id": user_id})
