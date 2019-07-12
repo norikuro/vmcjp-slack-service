e1955d466182c8301bae90bf731b7a0351916010
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-12 16:13:01 +0900

    Update slack_message.py

--- vmcjp/slack_message.py
+++ vmcjp/slack_message.py
@@ -6,17 +6,37 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def ask_wait_to_finish_task_message(event):
-  response = post_text(
-    event,
-    "Creating sddc now, please wait until the task is finished.",
-    "bot"
-  )
-#  logging.info(response.read())
+    response = post_text( 
+        event,
+        "Creating sddc now, please wait until the task is finished.",
+        "bot"
+    )
+#    logging.info(response.read())
 
 def ask_register_token_message(event):
-  response = post_text(
-    event,
-    "Please register VMC reresh token at first, type `register token`.",
-    "bot"
-  )
-#  logging.info(response.read())
+    response = post_text(
+        event,
+        "Please register VMC reresh token at first, type `register token`.",
+        "bot"
+    )
+#    logging.info(response.read())
+
+def start_create_sddc_wizard_message(event):
+    response = post_text(
+        event,
+        "OK, starting create sddc wizard.",
+        "bot"
+    )
+#    logging.info(response.read())
+    response = post_text(
+        event, 
+        "This conversation will end by typing `cancel` or doing nothing for 5 minutes", 
+        "bot"
+    )
+#    logging.info(response.read())
+    response = post_text(
+        event,
+        "Checking current resources...",
+        "bot"
+    )
+#    logging.info(response.read())
