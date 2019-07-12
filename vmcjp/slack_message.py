8105367db871fb4c477d44f15cf0f5ed4ec3cf26
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-12 16:04:33 +0900

    Update slack_message.py

--- vmcjp/slack_message.py
+++ vmcjp/slack_message.py
@@ -1,4 +1,9 @@
+import logging
+
 from vmcjp.utils.slack_post import post_text, post_button, post_option, post_field_button
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def ask_register_token_message(event):
   response = post_text(
