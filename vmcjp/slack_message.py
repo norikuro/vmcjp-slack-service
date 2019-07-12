f4ebde556cdc5a7559d087a414e1fa405acc1dc8
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-12 15:58:41 +0900

    Update slack_message.py

--- vmcjp/slack_message.py
+++ vmcjp/slack_message.py
@@ -1,3 +1,9 @@
-aaa
+from vmcjp.utils.slack_post import post_text, post_button, post_option, post_field_button
 
-def ask_register_token_message():
+def ask_register_token_message(event):
+  response = post_text(
+    event,
+    "Please register VMC reresh token at first, type `register token`.",
+    "bot"
+  )
+#  logging.info(response.read())
