05b1371c74719a8bd3fd419724f746668967c50e
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-12 16:07:31 +0900

    Update slack_message.py

--- vmcjp/slack_message.py
+++ vmcjp/slack_message.py
@@ -5,6 +5,14 @@ from vmcjp.utils.slack_post import post_text, post_button, post_option, post_fie
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+def ask_wait_to_finish_task(event):
+  response = post_text(
+    event,
+    "Creating sddc now, please wait until the task is finished.",
+    "bot"
+  )
+#  logging.info(response.read())
+
 def ask_register_token_message(event):
   response = post_text(
     event,
