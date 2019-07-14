14ae6a387d0ab37046f8eaed4e6fa711175b2c49
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-15 08:48:05 +0900

    Update slack_post.py

--- vmcjp/utils/slack_post.py
+++ vmcjp/utils/slack_post.py
@@ -78,20 +78,21 @@ def post_text(event, text, type="response"):
     return response
 
 def post_option(event, button, option_list, type="response"):
-    data = {
-        "token": event["slack_token"],
-        "channel": event["channel"]
-    }
+#    data = {
+#        "token": event["slack_token"],
+#        "channel": event["channel"]
+#    }
     button_set = json.load(open(button, 'r'))
     button_set["attachments"][0]["actions"][0].update(
         {"options": option_list}
     )
-    data.update(button_set)
+#    data.update(button_set)
+    response = post_text(event, button_set, type)
 
-    if "response" in type:
-        response = post_to_response_url(event["response_url"], data)
-    else:
-        response = post(event["post_url"], data, event["bot_token"])
+#    if "response" in type:
+#        response = post_to_response_url(event["response_url"], data)
+#    else:
+#        response = post(event["post_url"], data, event["bot_token"])
     return response
 
 def post_button(event, button, type="response"):
