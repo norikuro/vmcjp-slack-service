bd4b7acf6ff4cd2c4b1c45eb6e383109c3ed66a6
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-15 08:53:19 +0900

    Update slack_post.py

--- vmcjp/utils/slack_post.py
+++ vmcjp/utils/slack_post.py
@@ -78,21 +78,22 @@ def post_text(event, text, type="response"):
     return response
 
 def post_option(event, button, option_list, type="response"):
-#    data = {
-#        "token": event["slack_token"],
-#        "channel": event["channel"]
-#    }
+    data = {
+        "token": event["slack_token"],
+        "channel": event["channel"]
+    }
     button_set = json.load(open(button, 'r'))
     button_set["attachments"][0]["actions"][0].update(
         {"options": option_list}
     )
-#    data.update(button_set)
-    response = post_text(event, button_set, type)
+    data.update(button_set)
+#    response = post_text(event, button_set, type)
 
-#    if "response" in type:
+    if "response" in type:
 #        response = post_to_response_url(event["response_url"], data)
-#    else:
-#        response = post(event["post_url"], data, event["bot_token"])
+        response = post(event["response_url"], data)
+    else:
+        response = post(event["post_url"], data, event["bot_token"])
     return response
 
 def post_button(event, button, type="response"):
@@ -105,7 +106,8 @@ def post_button(event, button, type="response"):
     data.update(button_set)
 
     if "response" in type:
-        response = post_to_response_url(event["response_url"], data)
+        response = post(event["response_url"], data)
+#        response = post_to_response_url(event["response_url"], data)
     else:
         response = post(event["post_url"], data, event["bot_token"])
     return response
@@ -148,9 +150,11 @@ def post_field_button(event, button, pretext=None, type="response"):
     data.update(button_set)
     
     if "response" in type:
-        response = post_to_response_url(event["response_url"], data)
+        response = post(event["response_url"], data)
+#        response = post_to_response_url(event["response_url"], data)
     elif "webhook" in type:
-        response = post_to_response_url(event["webhook_url"], data)
+#        response = post_to_response_url(event["webhook_url"], data)
+        response = post(event["webhook_url"], data)
     else:
         response = post(event["post_url"], data, event["bot_token"])
     return response
