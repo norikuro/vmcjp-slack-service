db17635ba58760cebd51f8c1c904a32e91467808
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-15 17:25:55 +0900

    Update slack_message.py

--- vmcjp/slack_message.py
+++ vmcjp/slack_message.py
@@ -10,6 +10,8 @@ PRECHECK_BUTTON = constant.BUTTON_DIR + "precheck.json"
 DELETE_BUTTON = constant.BUTTON_DIR + "delete.json"
 LIST_BUTTON = constant.BUTTON_DIR + "list.json"
 HELP_BUTTON = constant.BUTTON_DIR + "help.json"
+LINK_AWS_BUTTON = constant.BUTTON_DIR + "link_aws.json"
+SINGLE_MULTI_BUTTON = constant.BUTTON_DIR + "single_multi.json"
 
 def help_message(event):
     response = post_button(event, HELP_BUTTON, "bot")
@@ -47,6 +49,15 @@ def delete_token_message(event):
     )
 #    logging.info(response.read())
 
+def cancel_token_registration_message(event):
+    response = post_text(
+        event,
+        "Canceled to register VMC refresh token.",
+        "bot"
+    )
+#    logging.info(response.read())
+
+
 def delete_sddc_message(event):
     response = post_option(
         event,
@@ -76,6 +87,14 @@ def start_create_sddc_wizard_message(event):
     )
 #    logging.info(response.read())
 
+def cancel_sddc_creation_message(event):
+    response = post_text(
+        event,
+        "OK, canceled a create sddc wizard.",
+        "bot"
+    )
+#    logging.info(response.read())
+
 def no_enough_resouces_message(event):
     response = post_text(
         event,
@@ -100,6 +119,31 @@ def max_hosts_message(event):
     )
 #    logging.info(response.read())
     response = post_button(event, PRECHECK_BUTTON, "bot")
+#    logging.info(response.read())
+
+def link_aws_message(event):
+    response = post_button(event, LINK_AWS_BUTTON, "bot")
+#    logging.info(response.read())
+
+def single_multi_message(event):
+    response = post_button(event, SINGLE_MULTI_BUTTON, "bot")
+#    logging.info(response.read())
+
+
+def wrong_network_message(event):
+    response = post_text(
+        event,
+        "Please enter correct network cidr block.",
+        "bot"
+    )
+#    logging.info(response.read())
+
+def create_sddc_confirmation_message(event):
+    response = post_field_button(
+        event, 
+        CREATE_BUTTON, 
+        type="bot"
+    )
 #    logging.info(response.read())
 
 def list_sddcs_text_message(event):
