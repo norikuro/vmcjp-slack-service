b3123ed325f6c0701180b6c71e8006c270a69cd7
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-16 19:53:48 +0900

    Update dbutils2.py

--- vmcjp/utils/dbutils2.py
+++ vmcjp/utils/dbutils2.py
@@ -67,3 +67,43 @@ class DocmentDb(object):
     def init_sddc_db():
         self.sddc_db = self.client[constant.SDDC_DB]
         self.sddc_col = self.event_db[constant.SDDC_COLLECTION]
+
+    def get_backedup_sddc_config(db):
+        config = db.find_with_fields(
+            {},
+            {
+                "org.id": 1,
+                "sddc_updated": 1,
+                "sddc.id": 1,
+                "sddc.name": 1,
+                "sddc.region": 1,
+                "sddc.num_hosts": 1,
+                "sddc.vpc_cidr": 1,
+                "org.display_name": 1,
+                "customer_vpc.linked_account": 1,
+                "customer_vpc.linked_vpc_subnets_id": 1,
+                "aws_connected_account": 1,
+                "_id": 0
+            }
+        )
+        
+        ca = config["aws_connected_account"]
+        for a in ca:
+            if config["customer_vpc"]["linked_account"] == a["account_number"]:
+                a_id = a["id"]
+        
+        return {
+            "org_id": config["org"]["id"],
+            "org_name": config["org"]["display_name"],
+            "updated": config["sddc_updated"],
+            "sddc_id": config["sddc"]["id"],
+            "sddc_name": config["sddc"]["name"],
+            "region": config["sddc"]["region"],
+            "num_hosts": config["sddc"]["num_hosts"],
+            "vpc_cidr": config["sddc"]["vpc_cidr"],
+            "aws_account": config["customer_vpc"]["linked_account"],
+            "customer_subnet_id": config["customer_vpc"]["linked_vpc_subnets_id"],
+            "connected_account_id": a_id,
+            "link_aws": "True"
+        }
+
