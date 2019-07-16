b451b81475d2f8644211d3cc6fca1776c52af566
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-16 16:22:13 +0900

    Update slack_event.py

--- vmcjp/slack_event.py
+++ vmcjp/slack_event.py
@@ -97,7 +97,7 @@ def list_sddcs(vmc_client, token, org_id):
     ]
 
 def event_handler(event):
-    int_status = ["resource_check", "sddc_name", "single_multi", "num_hosts", "aws_account", "vpc", "vpc_cidr", "link_aws"]
+    int_status = ["create_sddc", "resource_check", "sddc_name", "single_multi", "num_hosts", "aws_account", "vpc", "vpc_cidr", "link_aws"]
     text = event.get("text").lower()
     
     db = dbutils2.DocmentDb(event.get("db_url"))
