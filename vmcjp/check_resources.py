7b43d7dee1bc5a23fb6d885d76f019a6b0bf251b
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-16 22:28:34 +0900

    Update check_resources.py

--- vmcjp/check_resources.py
+++ vmcjp/check_resources.py
@@ -7,7 +7,7 @@ import os
 from vmware.vapi.vmc.client import create_vmc_client
 from vmcjp.utils.lambdautils import call_lambda
 from vmcjp.utils.s3utils import read_json_from_s3
-from vmcjp.utils import dbutils
+from vmcjp.utils import dbutils2
 from vmcjp.utils import constant
 from vmcjp import slack_message
 
@@ -22,14 +22,26 @@ def get_vmc_client(token):
   
 def check(event, vmc_client):
   if not check_sddc(event.get("org_id"), event.get("sddc_id"), vmc_client):
-    return "SDDC ID {} already exists".format(event.get("sddc_id"))
+    return {
+      "result": False,
+      "message": "SDDC ID {} already exists".format(event.get("sddc_id"))
+    }
   if not check_num_hosts(event.get("org_id"), event.get("num_hosts"), vmc_client):
-    return "Maximum deployable number of hosts exeeded"
+    return {
+      "result": False,
+      "message": "Maximum deployable number of hosts exeeded"
+    }
   if not check_customer_aws(event.get("org_id"), event.get("aws_account"), vmc_client):
-    return "AWS Account with ID {} doesn't exist or connected to this Org".format(event.get("aws_account"))
+    return {
+      "result": False,
+      "message": "AWS Account with ID {} doesn't exist or connected to this Org".format(event.get("aws_account"))
+    }
   else:
 #    call_lambda("create_sddc", event)
-    return "Checked, no pronlem. Then I will create sddc."
+    return {
+      "result": True,
+      "message": "Checked, no pronlem. Then I will create sddc."
+    }
 
 def check_sddc(org_id, sddc_id, vmc_client):
 # check if sddc name and id is not already exist
@@ -71,12 +83,26 @@ def lambda_handler(event, context):
 #  logging.info(event)
   event.update({"org_id": os.environ["test_org"]}) #for test
 
+  result = check(
+    event, 
+    get_vmc_client(event.get("token"))
+  )
+  
   event.update(
     {
-      "check_result": check(
-        event, 
-        get_vmc_client(event.get("token"))
-      )
+      "check_result": result.get("message")
     }
   )
   slack_message.check_result_message(event)
+  
+  db = dbutils2.DocmentDb(event.get("db_url"))
+  if result.get("result"):
+    db.write_event_db(
+      event.get("user_id"), 
+      {
+        "command": "create",
+        "status": "creating"
+      }
+    )
+  else:
+    db.delete_event_db(user_id)
