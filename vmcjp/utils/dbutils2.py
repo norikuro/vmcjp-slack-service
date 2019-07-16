90a10704a32bdc28662ac5240a7f583a20c2fd47
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-16 21:01:35 +0900

    Update dbutils2.py

--- vmcjp/utils/dbutils2.py
+++ vmcjp/utils/dbutils2.py
@@ -64,7 +64,7 @@ class DocmentDb(object):
     def delete_cred_db(self, user_id):
         self.cred_col.remove({"_id": user_id})
 
-    def init_sddc_db():
+    def init_sddc_db(self):
         sddc_db = self.client[constant.SDDC_DB]
         self.sddc_col = sddc_db[constant.SDDC_COLLECTION]
 
