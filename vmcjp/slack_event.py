00cf3edc4bce63d1aae6552f7f102a145339e9e3
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-21 21:58:38 +0900

    Update slack_event.py

--- vmcjp/slack_event.py
+++ vmcjp/slack_event.py
@@ -26,11 +26,11 @@ def get_max_num_hosts(token, org_id):
     for sddc in sddcs:
         i += len(sddc.resource_config.esx_hosts)
     max_hosts = (int(vmc_client.Orgs.get(org_id).properties.values["sddcLimit"]) - 1) - i
-#    if max_hosts < 1:
-#        return max_hosts
-#    else:
-#        return 1 if max_hosts < 3 else max_hosts
-    return 4 #for test
+    if max_hosts < 1:
+        return max_hosts
+    else:
+        return 1 if max_hosts < 3 else max_hosts
+#    return 4 #for test
 
 def get_max_num_hosts_zerocloud(token, org_id): #for internal use
     vmc_client = get_vmc_client(token)
