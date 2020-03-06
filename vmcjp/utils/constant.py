a3cc4556c148ea270a70c90db91e0a8963683671
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-06 19:27:42 +0900

    Update constant.py

--- vmcjp/utils/constant.py
+++ vmcjp/utils/constant.py
@@ -38,6 +38,7 @@ class _const:
   NOT_ENOUGH = "no_enough_resouces_message"
   MAX_HOSTS = "max_hosts_message"
   REGION = "region_list_message"
+  SDDC_NAME = "ask_sddc_name_message"
 
   class ConstError(TypeError):
     pass
