7d447aed7d315cdad3f9648ffbe5b3a64ca30f46
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-06 19:11:48 +0900

    Update constant.py

--- vmcjp/utils/constant.py
+++ vmcjp/utils/constant.py
@@ -31,6 +31,7 @@ class _const:
   CONFIRM_DELETE = "sddc_deletion_confirmation_message"
   START_DELETE = "started_delete_sddc_message"
   CANT_DELETE = "cannot_delete_sddc_message"
+  CALCEL_DELETE = "cancel_sddc_deletion_message"
 
   class ConstError(TypeError):
     pass
