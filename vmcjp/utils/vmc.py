d08443ecc3e44d05d6a4dabd54745d00b0bb9582
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-06 11:58:14 +0900

    Update vmc.py

--- vmcjp/utils/vmc.py
+++ vmcjp/utils/vmc.py
@@ -1,9 +1,12 @@
 import requests
 import json
+import logging
 
 BASE_URL = "https://console.cloud.vmware.com/csp/gateway"
 HEADERS = {"Content-Type": "application/json"}
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def login(token):
   uri = "/am/api/auth/api-tokens/details"
@@ -19,6 +22,9 @@ def login(token):
 
 def validate_token(token, org_id):
     response = login(token)
+    
+    logging.info("status code: {}".format(response.status_code))
+    logging.info(response.json())
     
     if response.status_code == 200:
       data = response.json()
