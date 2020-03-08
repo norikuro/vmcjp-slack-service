6ffc9a74e54947051d1e467da7421db8143e70b4
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-08 20:59:58 +0900

    Update interactive.py

--- vmcjp/slack/interactive.py
+++ vmcjp/slack/interactive.py
@@ -22,26 +22,26 @@ from vmcjp.vmc.vmc_client import get_vmc_client, list_region, list_aws_account
 #        } for account in accounts
 #    ]
 
-def list_vpc(
-    vmc_client, 
-    org_id, 
-    linked_account_id, 
-    region
-):
-    csbnts = vmc_client.orgs.account_link.CompatibleSubnets.get(
-        org_id, 
-        linked_account_id=linked_account_id, 
-        region=region, 
-        sddc=None, 
-        force_refresh=None
-    )
-    vpcs = csbnts.vpc_map.keys()
-    return [
-        {
-            "text": vpc,
-            "value": vpc
-        } for vpc in vpcs
-    ]
+#def list_vpc(
+#    vmc_client, 
+#    org_id, 
+#    linked_account_id, 
+#    region
+#):
+#    csbnts = vmc_client.orgs.account_link.CompatibleSubnets.get(
+#        org_id, 
+#        linked_account_id=linked_account_id, 
+#        region=region, 
+#        sddc=None, 
+#        force_refresh=None
+#    )
+#    vpcs = csbnts.vpc_map.keys()
+#    return [
+#        {
+#            "text": vpc,
+#            "value": vpc
+#        } for vpc in vpcs
+#    ]
 
 def list_subnet(
     vmc_client,
