8d15e3a5329542e596db1d83d6de75af3f9b76aa
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-08 20:48:26 +0900

    Update vmc_client.py

--- vmcjp/vmc/vmc_client.py
+++ vmcjp/vmc/vmc_client.py
@@ -37,15 +37,15 @@ def get_max_num_hosts(token, org_id):
 #    vmc_client = get_vmc_client(token)
 #    return int(vmc_client.Orgs.get(org_id).properties.values["maxHostsPerSddcOnCreate"])
 
-def list_region(token, org_id):
-    vmc_client = get_vmc_client(token)
-    regions = vmc_client.Orgs.get(org_id).properties.values.get("defaultAwsRegions").split(",")
-    return [
-        {
-            "text": region,
-            "value": region
-        } for region in regions
-    ]
+#def list_region(token, org_id):
+#    vmc_client = get_vmc_client(token)
+#    regions = vmc_client.Orgs.get(org_id).properties.values.get("defaultAwsRegions").split(",")
+#    return [
+#        {
+#            "text": region,
+#            "value": region
+#        } for region in regions
+#    ]
 
 def is_network(address):
     try:
