c77e6bd52ee509ce6a46bb1c44093da923b7e8f2
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-08 20:40:36 +0900

    Update event.py

--- vmcjp/slack/event.py
+++ vmcjp/slack/event.py
@@ -1,9 +1,3 @@
-#import logging
-#import ipaddress
-#import atexit
-#import requests
-
-#from vmware.vapi.vmc.client import create_vmc_client
 from vmcjp.utils.loginutils import validate_token
 from vmcjp.utils import dbutils
 from vmcjp.utils import constant
@@ -12,94 +6,6 @@ from vmcjp.vmc.vmc_client import get_vmc_client, list_sddcs, get_max_num_hosts, 
 
 #logger = logging.getLogger()
 #logger.setLevel(logging.INFO)
-
-#def get_vmc_client(token):
-#    session = requests.Session()
-#    vmc_client = create_vmc_client(token, session=session)
-#    atexit.register(session.close)
-#    return vmc_client
-
-#def get_max_num_hosts(token, org_id):
-## get deployable number of hosts
-## 1 host is for Storage EDRS
-    
-#    vmc_client = get_vmc_client(token)
-    
-#    sddcs = vmc_client.orgs.Sddcs.list(org_id)
-    
-#    i = 0
-#    for sddc in sddcs:
-#        i += len(sddc.resource_config.esx_hosts)
-#    max_hosts = (int(vmc_client.Orgs.get(org_id).properties.values["sddcLimit"]) - 1) - i
-##    if max_hosts < 1:
-##        return max_hosts
-##    else:
-##        return 1 if max_hosts < 3 else max_hosts
-#    return 4 #for test
-
-##def get_max_num_hosts_zerocloud(token, org_id): #for internal use
-##    vmc_client = get_vmc_client(token)
-##    return int(vmc_client.Orgs.get(org_id).properties.values["maxHostsPerSddcOnCreate"])
-
-#def list_region(vmc_client, org_id):
-#    regions = vmc_client.Orgs.get(org_id).properties.values.get("defaultAwsRegions").split(",")
-#    return [
-#        {
-#            "text": region,
-#            "value": region
-#        } for region in regions
-#    ]
-
-#def is_network(address):
-#    try:
-#        ipaddress.ip_network(address)
-#        return True
-#    except ValueError:
-#        return False
-
-#def is_valid_network(address):
-#    try:
-#        nw = ipaddress.ip_network(address)
-#    except ValueError:
-#        return False
-    
-#    prefix = nw.prefixlen
-#    if prefix != 23 and prefix != 20 and prefix != 16:
-#        return False
-    
-#    rs10 = ipaddress.ip_network(u'10.0.0.0/15')
-#    rs192 = ipaddress.ip_network(u'192.168.1.0/24')
-#    rs172 = ipaddress.ip_network(u'172.31.0.0/16')
-
-#    if nw.subnet_of(rs10):
-#        return False
-#    elif rs192.subnet_of(nw):
-#        return False
-#    elif nw.subnet_of(rs172):
-#        return False
-    
-#    nw10 = ipaddress.ip_network(u'10.0.0.0/8')
-#    nw172 = ipaddress.ip_network(u'172.16.0.0/12')
-#    nw192 = ipaddress.ip_network(u'192.168.0.0/16')
-    
-#    if nw.subnet_of(nw10):
-#        return True
-#    elif nw.subnet_of(nw172):
-#        return True
-#    elif nw.subnet_of(nw192):
-#        return True
-#    else:
-#        return False
-
-#def list_sddcs(token, org_id):
-#    vmc_client = get_vmc_client(token)
-#    sddcs = vmc_client.orgs.Sddcs.list(org_id)
-#    return [
-#        {
-#            "text": sddc.name,
-#            "value": "{}+{}".format(sddc.name, sddc.id)
-#        } for sddc in sddcs
-#    ]
 
 def event_cred_update(event, cred):
     event.update(
