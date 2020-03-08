7c08448a68f8c9b3bb053863c8b3edc066de51bf
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-08 20:08:26 +0900

    Update event.py

--- vmcjp/slack/event.py
+++ vmcjp/slack/event.py
@@ -19,27 +19,27 @@ from vmcjp.vmc.vmc_client import get_vmc_client, list_sddcs
 #    atexit.register(session.close)
 #    return vmc_client
 
-def get_max_num_hosts(token, org_id):
-# get deployable number of hosts
-# 1 host is for Storage EDRS
+#def get_max_num_hosts(token, org_id):
+## get deployable number of hosts
+## 1 host is for Storage EDRS
     
-    vmc_client = get_vmc_client(token)
-    
-    sddcs = vmc_client.orgs.Sddcs.list(org_id)
-    
-    i = 0
-    for sddc in sddcs:
-        i += len(sddc.resource_config.esx_hosts)
-    max_hosts = (int(vmc_client.Orgs.get(org_id).properties.values["sddcLimit"]) - 1) - i
-#    if max_hosts < 1:
-#        return max_hosts
-#    else:
-#        return 1 if max_hosts < 3 else max_hosts
-    return 4 #for test
-
-#def get_max_num_hosts_zerocloud(token, org_id): #for internal use
 #    vmc_client = get_vmc_client(token)
-#    return int(vmc_client.Orgs.get(org_id).properties.values["maxHostsPerSddcOnCreate"])
+    
+#    sddcs = vmc_client.orgs.Sddcs.list(org_id)
+    
+#    i = 0
+#    for sddc in sddcs:
+#        i += len(sddc.resource_config.esx_hosts)
+#    max_hosts = (int(vmc_client.Orgs.get(org_id).properties.values["sddcLimit"]) - 1) - i
+##    if max_hosts < 1:
+##        return max_hosts
+##    else:
+##        return 1 if max_hosts < 3 else max_hosts
+#    return 4 #for test
+
+##def get_max_num_hosts_zerocloud(token, org_id): #for internal use
+##    vmc_client = get_vmc_client(token)
+##    return int(vmc_client.Orgs.get(org_id).properties.values["maxHostsPerSddcOnCreate"])
 
 def list_region(vmc_client, org_id):
     regions = vmc_client.Orgs.get(org_id).properties.values.get("defaultAwsRegions").split(",")
