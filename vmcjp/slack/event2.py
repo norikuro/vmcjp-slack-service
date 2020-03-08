3ecfb91ccfee30fdd0079f8993f909a0c1528a15
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-09 08:06:23 +0900

    Update event2.py

--- vmcjp/slack/event2.py
+++ vmcjp/slack/event2.py
@@ -4,3 +4,12 @@ from vmcjp.utils import constant
 def event_handler(event):
     text = event.get("text").lower()
     
+    db = dbutils.DocmentDb(event.get("db_url"))
+    current = db.read_event_db(event.get("user_id"), 120)
+    if current is not None and current.get("status") == "creating":
+        message_handler(constant.ASK_WAIT_TASK, event)
+        return
+    
+    result = db.read_event_db(event.get("user_id"), 5)
+    __cred_data = db.read_cred_db(event.get("user_id"))
+    
