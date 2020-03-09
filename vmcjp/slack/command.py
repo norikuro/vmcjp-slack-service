bdec615663c420f0f34313cd91689d82f93f9294
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-09 15:42:37 +0900

    Update command.py

--- vmcjp/slack/command.py
+++ vmcjp/slack/command.py
@@ -20,7 +20,7 @@ def register_org(event, db):
     db.write_cred_db(
         event.get("user_id"), 
         {
-            "status": "register_token"
+            "status": "registering"
         }
     )
 
