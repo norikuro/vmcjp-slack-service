97468fc5861deb9c7bc5107c47a5d22071a05cd6
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-09 10:30:04 +0900

    Update event2.py

--- vmcjp/slack/event2.py
+++ vmcjp/slack/event2.py
@@ -1,5 +1,6 @@
 from vmcjp.utils import dbutils
 from vmcjp.utils import constant
+from vmcjp.slack.messages import message_handler
 
 def event_handler(event):
     text = event.get("text").lower()
@@ -10,5 +11,16 @@ def event_handler(event):
         message_handler(constant.ASK_WAIT_TASK, event)
         return
     
-    result = db.read_event_db(event.get("user_id"), 5)
+    event_db = db.read_event_db(event.get("user_id"), 5)
     cred = db.read_cred_db(event.get("user_id"))
+    if event_db is None:
+        if text in constant.COMMAND_ORG:
+            eval(constant.COMMAND_ORG[text])(event)
+            return
+        elif text in constant.COMMAND_SDDC:
+            if cred is not None:
+                eval(constant.COMMAND_SDDC[text])(event)
+                return
+            else:
+                message_handler(constant.ASK_REGISTER_TOKEN, event)
+                return
