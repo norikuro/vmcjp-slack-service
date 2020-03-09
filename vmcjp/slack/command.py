a44a969ec19f9218e4f02f1e23102edd971ee68f
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-09 12:29:54 +0900

    Update command.py

--- vmcjp/slack/command.py
+++ vmcjp/slack/command.py
@@ -4,16 +4,20 @@ def command_handler(cmd, event, db):
     eval(cmd)(event, db)
 
 def register_org(event, db):
-  message_handler(constant.REGISTER_ORG, event)
-  db.write_cred_db(
-    event.get("user_id"), 
-    {
-      "status": "registering"
-    }
-  )
-  db.write_event_db(
-    event.get("user_id"), 
-    {
-      "command": "register org",
-    }
-  )
+    message_handler(constant.REGISTER_ORG, event)
+    db.write_cred_db(
+        event.get("user_id"),
+        {
+            "status": "registering"
+        }
+    )
+    db.write_event_db(
+        event.get("user_id"), 
+        {
+            "command": "register org",
+        }
+    )
+
+def delete_org(event, db):
+    message_handler(constant.DELETE_ORG, event)
+    db.delete_cred_db(event["user_id"])
