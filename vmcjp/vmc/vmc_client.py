ed99928718c794f3a7a97d18e42fc9fd9fdce51e
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-10 15:11:30 +0900

    Update vmc_client.py

--- vmcjp/vmc/vmc_client.py
+++ vmcjp/vmc/vmc_client.py
@@ -5,7 +5,7 @@ from vmware.vapi.vmc.client import create_vmc_client
 def get_vmc_client(token):
     return create_vmc_client(token)
 
-def list_sddcs(token, org_id):
+def list_sddcs__(token, org_id):
     vmc_client = get_vmc_client(token)
     sddcs = vmc_client.orgs.Sddcs.list(org_id)
     return [
