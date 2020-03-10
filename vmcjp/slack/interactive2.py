31c4e8d7f036a1058a2474e54d09a9802e624a66
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-10 14:50:20 +0900

    Update interactive2.py

--- vmcjp/slack/interactive2.py
+++ vmcjp/slack/interactive2.py
@@ -1,1 +1,10 @@
-aaaa
+def interactive_handler(event):
+    user_id = event.get("user_id")
+    
+    db = dbutils.DocmentDb(event.get("db_url"))
+    event_db = db.read_event_db(user_id, 5)
+    
+    if result is None:
+      message_handler(constant.MAY_I, event)
+      return
+    
