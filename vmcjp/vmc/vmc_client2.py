65de781d7a50358d5f20680d01b0139424c36b7e
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-11 12:19:58 +0900

    Update vmc_client2.py

--- vmcjp/vmc/vmc_client2.py
+++ vmcjp/vmc/vmc_client2.py
@@ -5,9 +5,9 @@ LOGIN_BASE_URL = "https://console.cloud.vmware.com/csp/gateway"
 BASE_URL = "https://vmc.vmware.com"
 HEADERS = {"Content-Type": "application/json"}
 
-def login(token):
+def login(refresh_token):
   uri = "/am/api/auth/api-tokens/authorize"
-  params = {"refresh_token": token}
+  params = {"refresh_token": refresh_token}
   
   response = requests.post(
     '{}{}'.format(LOGIN_BASE_URL, uri),
