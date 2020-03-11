cf87f0e66e309642946eafee1d922686d68c080d
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-11 12:16:52 +0900

    Update vmc_client2.py

--- vmcjp/vmc/vmc_client2.py
+++ vmcjp/vmc/vmc_client2.py
@@ -1,1 +1,24 @@
-aaaaa
+import requests
+import json
+
+LOGIN_BASE_URL = "https://console.cloud.vmware.com/csp/gateway"
+BASE_URL = "https://vmc.vmware.com"
+HEADERS = {"Content-Type": "application/json"}
+
+def login(token):
+  uri = "/am/api/auth/api-tokens/authorize"
+  params = {"refresh_token": token}
+  
+  response = requests.post(
+    '{}{}'.format(LOGIN_BASE_URL, uri),
+    headers=HEADERS,
+    params = params
+  )
+  
+  if response.status_code == 200:
+    data = response.json()
+    retrun {
+      "access_token": data.get("access_token"),
+      "expires": data.get("expires_in")
+    }
+  
