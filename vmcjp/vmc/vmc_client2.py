5929ce5ee5975f3a5a966f36f98435f3e8436668
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-11 16:39:13 +0900

    Update vmc_client2.py

--- vmcjp/vmc/vmc_client2.py
+++ vmcjp/vmc/vmc_client2.py
@@ -7,23 +7,33 @@ BASE_URL = "https://vmc.vmware.com"
 HEADERS = {"Content-Type": "application/json"}
 
 def login(refresh_token):
-  uri = "/am/api/auth/api-tokens/authorize"
-  params = {"refresh_token": refresh_token}
-  
-  response = requests.post(
-    '{}{}'.format(LOGIN_BASE_URL, uri),
-    headers=HEADERS,
-    params = params
-  )
-  now = time.time()
-  
-  if response.status_code == 200:
-    data = response.json()
-    expire_in = data.get("expires_in")
-    expire_time = now + expire_in
-    retrun {
-      "access_token": data.get("access_token"),
-      "expires_in": expire_in,
-      "expire_time": expire_time
-    }
-  
+    uri = "/am/api/auth/api-tokens/authorize"
+    params = {"refresh_token": refresh_token}
+    
+    response = requests.post(
+        '{}{}'.format(LOGIN_BASE_URL, uri),
+        headers=HEADERS,
+      params = params
+    )
+    
+    now = time.time()
+    
+    if response is not None:
+        status_code = response.status_code
+        data = response.json()
+        if status_code == 200:
+            expire_in = data.get("expires_in")
+            expire_time = now + expire_in
+            retrun {
+                "access_token": data.get("access_token"),
+                "expires_in": expire_in,
+                "expire_time": expire_time
+            }
+        elif status_code==400:
+            message = {
+                "status_code": status_code,
+                "message": data.get("message")
+            }
+            raise Exception(message)
+    else:
+        raise Exception("Cannot connect VMC, Something wrong!")
